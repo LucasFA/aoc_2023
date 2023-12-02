41caf2a6157286edd6b0610fa58ea5e0aa6d6a23
LucasFA <23667494+LucasFA@users.noreply.github.com>
2023-12-02 20:38:11 +0100

    Day 1 part 2, but wrong

--- day_01/src/main.rs
+++ day_01/src/main.rs
@@ -1,4 +1,4 @@
-use std::iter::zip;
+use regex::Regex;
 
 fn main() {
     const INPUT: &str = include_str!("./input");
@@ -21,32 +21,50 @@ fn parse_line_part1(s: &str) -> u32 {
     return charfinal.parse::<u32>().unwrap();
 }
 
-fn parse_line_part2(s: &str) -> u32 {
-    const DICTIONARY: [&str; 9] = [
-        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
-    ];
-    let mut sstring = s.to_string();
-    for (idx, num_name) in zip(1..=9, DICTIONARY) {
-        dbg!(idx.clone());
-        dbg!(num_name.clone());
-        dbg!(sstring.clone());
-
-        sstring = sstring.replace(num_name, idx.to_string().as_str());
-    }
-
-    dbg!(&sstring);
-    parse_line_part1(sstring.as_str())
-}
-
 fn part2(s: &str) -> u32 {
     s.split_ascii_whitespace().map(parse_line_part2).sum()
 }
+
+fn parse_line_part2(s: &str) -> u32 {
+    let dict: Vec<&str> = vec![
+        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    ];
+
+    // let r = Regex::new(r"^(?:(\d)?([A-Za-z])?)+$").unwrap(); // original
+    let r = Regex::new(r"(\d)|(one|two|three|four|five|six|seven|eight|nine)").unwrap();
+
+    let mut result = Vec::new();
+
+    for cap in r.captures_iter(s) {
+        let caps = cap
+            .iter()
+            .skip(1)
+            .filter(|c| c.is_some())
+            .map(|c| c.unwrap().as_str())
+            .next()
+            .unwrap();
+
+        if caps.len() == 1 {
+            result.push(
+                caps.parse::<u32>()
+                    .expect("Could not parse single character into u8"),
+            );
+        } else {
+            result.push(1 + dict.iter().position(|&number| number == caps).unwrap() as u32);
+        }
+    }
+
+    let dig1 = result[0] as u32 * 10 as u32;
+    let dig2 = *result.last().unwrap() as u32;
+    dig1 + dig2
+}
+
 #[cfg(test)]
 mod test {
     mod part1 {
         use crate::*;
         #[test]
-        fn full_part1_example() {
+        fn full_example() {
             let input = "
 1abc2
 pqr3stu8vwx
@@ -58,7 +76,7 @@ treb7uchet";
         }
 
         #[test]
-        fn individual1() {
+        fn simple() {
             let input = "1abc2";
             let result = part1(input);
 
@@ -88,6 +106,19 @@ zoneight234
             let input = "two1nine";
             let result = part2(input);
             assert_eq!(result, 29)
+        }
+
+        #[test]
+        fn overlapping() {
+            let input = "zoneight234";
+            let result = part2(input);
+            assert_eq!(result, 14)
+        }
+        #[test]
+        fn overlapping2() {
+            let input = "xtwone3four";
+            let result = part2(input);
+            assert_eq!(result, 24)
         }
     }
 }
